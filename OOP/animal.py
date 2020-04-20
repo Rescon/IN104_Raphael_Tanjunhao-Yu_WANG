338ac0618ffd127ccd34f28e789f13217cbbd74a
Rescon <yu.wang@ensta-paris.fr>
2020-04-20 14:46:17 +0200

    Files for OOP

--- OOP/animal.py
+++ OOP/animal.py
@@ -1,41 +1,58 @@
-#defines animal as highest superclass
+# Define exceptions
+class animalError(Exception): pass
+class OutOfRangeError(animalError): pass
+
+# defines animal as highest superclass
 class animal:
-    def __init__(self,name,weight,speed):
+    def __init__(self, name, weight, speed):
         self.name = name
         self.weight = weight
         self.speed = speed
+
     def getName(self):
         return self.name
+
     def getWeight(self):
+        if self.weight <= 0:
+            print("The weight should be positive.")
+            raise OutOfRangeError
         return self.weight
 
-    def getSpeed(speed_):
+    def getSpeed(self):
+        if self.speed <= 0:
+            print("The speed should be positive.")
+            raise OutOfRangeError
         return self.speed
 
+
 class Predator(animal):
-    #inherit all methods from animal
-    def fight(self,otherPredator):
-        #fight is won by weight
+    # inherit all methods from animal
+    def fight(self, otherPredator):
+        # fight is won by weight
         target = otherPredator.getWeight()
         if self.weight > target:
             return True
         else:
             return False
-    def chase(self,prey):
-        target = prey.getSpeed() #this is the speed to beat to catch prey
+
+    def chase(self, prey):
+        target = prey.getSpeed()  # this is the speed to beat to catch prey
         if self.speed > target:
-            return True          #true for when chase is successful
+            return True  # true for when chase is successful
         else:
             return False
+
+
 class Prey(animal):
-    def run(self, Predator):
-        threat = Predator.getSpeed()
+    def run(self, predator):
+        threat = predator.getSpeed()
         if self.speed >= threat:
             return True
         else:
-            return False 
-    def fight(self,Predator):
-        threat = Predator.getWeight
+            return False
+
+    def fight(self, predator):
+        threat = predator.getWeight()
         if self.weight >= threat:
             return True
         else:
--- OOP/animalKingdom.py
+++ OOP/animalKingdom.py
@@ -1,47 +1,48 @@
 import animal
 
-def encounter(animal1,animal2):
+
+def encounter(animal1, animal2):
     name1 = animal1.getName()
     name2 = animal2.getName()
-    print(name1+" meets "+name2)
-    if isinstance(animal1,animal.Predator):
+    print(name1 + " meets " + name2)
+    if isinstance(animal1, animal.Predator):
         if isinstance(animal2, animal.Predator):
-            #predator 1 will fight predator 2
-            print(name1 +" initiates the fight")
+            # predator 1 will fight predator 2
+            print(name1 + " initiates the fight")
             result = animal1.fight(animal2)
-            if result == False:
-                print(name1 +" lost the fight")
-                print(name2 +" ate " +name1)
+            if not result:
+                print(name1 + " lost the fight")
+                print(name2 + " ate " + name1)
             else:
-                print(name1 +" overpowered" +name2)
-                print(name1 +"ate " +name2)
-        if isinstance(animal2,animal.Prey):
-            #predator will chase predator 2
-            print(name1 +" starts chasing")
-            result  = animal1.chase
-            if result == True:
-                print(name1 +" overpowered the " +name2)
-                print(name1 +" ate " +name2)
+                print(name1 + " overpowered" + name2)
+                print(name1 + "ate " + name2)
+        if isinstance(animal2, animal.Prey):
+            # predator will chase predator 2
+            print(name1 + " starts chasing")
+            result = animal1.chase
+            if result:
+                print(name1 + " overpowered the " + name2)
+                print(name1 + " ate " + name2)
             else:
-                print("But "+name2+" was too fast")
-                print(name1 +" left emptyhanded")
+                print("But " + name2 + " was too fast")
+                print(name1 + " left emptyhanded")
     if isinstance(animal1, animal.Prey):
         if isinstance(animal2, animal.Predator):
-            print(name1 +" starts running")
+            print(name1 + " starts running")
             result = animal1.run(animal2)
-            if result == True:
-                print(name1 +" gets to see another day")
+            if result:
+                print(name1 + " gets to see another day")
             else:
-                print(name1 +" got eaten by "+ name2)
+                print(name1 + " got eaten by " + name2)
         if isinstance(animal2, animal.Prey):
             print("The 2 animals greeted each other and moved on with their day")
 
 
 if __name__ == '__main__':
-    #initialise afew animals
-    wolf = animal.Predator('wolf',30,50)
-    bear = animal.Predator('bear',50,30)
-    encounter(wolf,bear)
-    rabbit = animal.Prey('rabbit',20,50)
-    encounter(wolf,rabbit)
-    encounter(rabbit,rabbit)
+    # initialise a few animals
+    wolf = animal.Predator('wolf', 30, 50)
+    bear = animal.Predator('bear', 50, 30)
+    encounter(wolf, bear)
+    rabbit = animal.Prey('rabbit', 20, 50)
+    encounter(wolf, rabbit)
+    encounter(rabbit, rabbit)
